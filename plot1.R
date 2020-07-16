4be95e31eeb0b86419a19062152ed7473be6f002
Estevam Caixeta Martins Teixeira <12239379+estevamteixeira@users.noreply.github.com>
2020-07-16 16:58:28 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,52 @@
+##-----------------
+## Loading the Data
+##-----------------
+
+temp <- read.table(file.path(path, files[1]), header = TRUE, sep = ";",
+                   na.strings = "?",
+                   colClasses = c("character", "character", "numeric", "numeric",
+                                  "numeric", "numeric", "numeric", "numeric",
+                                  "numeric"))
+head(temp)
+# The dataset has 2,075,259 rows and 9 columns
+dim(temp)
+summary(temp)
+
+# date conversion
+
+temp$Date <- as.POSIXct(temp$Date, format="%d/%m/%Y")
+
+temp$Time <- format(temp$Time, format = "%H:%M:%S")
+
+## Filtering data from the dates 2007-02-01 and 2007-02-02
+
+data <- subset(temp, temp$Date >= "2007-02-01" & temp$Date <= "2007-02-02")
+head(data);dim(data)
+summary(data)
+
+# Creating a DateTime variable
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time, sep = " "),
+                            format="%Y-%m-%d %H:%M:%S")
+
+## Removing unused information to save memory
+
+rm(list = c("temp"))
+
+## Looking for missing information in each variable
+
+apply(sapply(data, is.na),2,sum)
+
+##-------
+## Plot 1
+##-------
+
+hist(data$Global_active_power,
+     xlab = "Global Active Power (in kilowatts)",
+     ylab = "Frequency",
+     main = "Global Active Power",
+     col = "red")
+
+## Save file as .png and close device
+
+dev.copy(png,'plot1.png', width = 480, height = 480)
+dev.off()
